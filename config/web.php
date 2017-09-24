bd820ce8a40538fe70facb4623b2727b93e139ca
Pavel Kaymakov <pavel.kaymakov@gmail.com>
2017-09-24 11:46:15 +0300

    Sitename

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require(__DIR__ . '/db.php');
 
 $config = [
     'id' => 'basic',
+    'name' => 'Интернет-магазин дрогери',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'language' => 'ru',
